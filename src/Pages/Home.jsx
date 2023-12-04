57c7b9554ef855ae52384a10949590c1210070e1
Cihsan <mutamuty@gmail.com>
2023-12-04 21:59:08 +0600

    [-] add Home Page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CourseList from '../redux/features/CourseList';
+
+const Home = () => {
+    return (
+        <>
+            <CourseList/>
+        </>
+    );
+};
+
+export default Home;
